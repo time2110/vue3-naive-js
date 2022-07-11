29d790f438b25f2a40f5f9b6cdd0aba7e8fc0f7f
pjw3859 <1395292110@qq.com>
2022-07-11 17:54:31 +0800

    安装rollup-plugin-visualizer

--- build/plugin/index.js
+++ build/plugin/index.js
@@ -1,12 +1,26 @@
 import vue from '@vitejs/plugin-vue'
-
+/**
+ * * 扩展setup插件，支持在script标签中使用name属性
+ * usage: <script setup name="MyComp"></script>
+ */
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+// rollup打包分析插件
+import visualizer from 'rollup-plugin-visualizer'
 
 export function createVitePlugins(viteEnv, isBuild) {
     const plugins = [
         vue(),
         VueSetupExtend(),
       ]
+      if (isBuild) {
+        plugins.push(
+          visualizer({
+            open: true,
+            gzipSize: true,
+            brotliSize: true,
+          })
+        )
+      }
     
       return plugins
 }
