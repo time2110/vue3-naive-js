f281cae4bfda1dee8149df973bffad7cf23794a2
pjw3859 <1395292110@qq.com>
2022-07-11 18:17:09 +0800

    安装eslint、prettier

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-<div p-24>
+  <div p-24>
     <p>
       文档：<a hover-decoration-underline c-blue href="https://uno.antfu.me/" target="_blank">https://uno.antfu.me/</a>
     </p>
@@ -18,50 +17,50 @@
         <div text-20 font-600>Flex布局</div>
         <div flex w-360 flex-wrap justify-around ml-15 p-10>
           <div w-50 h-50 b-1 rounded-5 flex justify-center items-center p-10 m-20>
-            <span w-6 h-6 rounded-3 bg-black></span>
+            <span w-6 h-6 rounded-3 bg-black />
           </div>
           <div w-50 h-50 b-1 rounded-5 flex justify-between p-10 m-20>
-            <span w-6 h-6 rounded-3 bg-black></span>
-            <span w-6 h-6 rounded-3 bg-black self-end></span>
+            <span w-6 h-6 rounded-3 bg-black />
+            <span w-6 h-6 rounded-3 bg-black self-end />
           </div>
           <div w-50 h-50 b-1 rounded-5 flex justify-between p-10 m-20>
-            <span w-6 h-6 rounded-3 bg-black></span>
-            <span w-6 h-6 rounded-3 bg-black self-center></span>
-            <span w-6 h-6 rounded-3 bg-black self-end></span>
+            <span w-6 h-6 rounded-3 bg-black />
+            <span w-6 h-6 rounded-3 bg-black self-center />
+            <span w-6 h-6 rounded-3 bg-black self-end />
           </div>
           <div w-50 h-50 b-1 rounded-5 flex justify-between p-10 m-20>
             <div flex flex-col justify-between>
-              <span w-6 h-6 rounded-3 bg-black></span>
-              <span w-6 h-6 rounded-3 bg-black></span>
+              <span w-6 h-6 rounded-3 bg-black />
+              <span w-6 h-6 rounded-3 bg-black />
             </div>
             <div flex flex-col justify-between>
-              <span w-6 h-6 rounded-3 bg-black></span>
-              <span w-6 h-6 rounded-3 bg-black></span>
+              <span w-6 h-6 rounded-3 bg-black />
+              <span w-6 h-6 rounded-3 bg-black />
             </div>
           </div>
           <div w-50 h-50 b-1 rounded-5 flex flex-col justify-between items-center p-10 m-20>
             <div flex w-full justify-between>
-              <span w-6 h-6 rounded-3 bg-black></span>
-              <span w-6 h-6 rounded-3 bg-black></span>
+              <span w-6 h-6 rounded-3 bg-black />
+              <span w-6 h-6 rounded-3 bg-black />
             </div>
-            <span w-6 h-6 rounded-3 bg-black></span>
+            <span w-6 h-6 rounded-3 bg-black />
             <div flex w-full justify-between>
-              <span w-6 h-6 rounded-3 bg-black></span>
-              <span w-6 h-6 rounded-3 bg-black></span>
+              <span w-6 h-6 rounded-3 bg-black />
+              <span w-6 h-6 rounded-3 bg-black />
             </div>
           </div>
           <div w-50 h-50 b-1 rounded-5 flex flex-col justify-between p-10 m-20>
             <div flex w-full justify-between>
-              <span w-6 h-6 rounded-3 bg-black></span>
-              <span w-6 h-6 rounded-3 bg-black></span>
+              <span w-6 h-6 rounded-3 bg-black />
+              <span w-6 h-6 rounded-3 bg-black />
             </div>
             <div flex w-full justify-between>
-              <span w-6 h-6 rounded-3 bg-black></span>
-              <span w-6 h-6 rounded-3 bg-black></span>
+              <span w-6 h-6 rounded-3 bg-black />
+              <span w-6 h-6 rounded-3 bg-black />
             </div>
             <div flex w-full justify-between>
-              <span w-6 h-6 rounded-3 bg-black></span>
-              <span w-6 h-6 rounded-3 bg-black></span>
+              <span w-6 h-6 rounded-3 bg-black />
+              <span w-6 h-6 rounded-3 bg-black />
             </div>
           </div>
         </div>
@@ -103,4 +102,3 @@
   background-color: $primaryColor;
 }
 </style>
-
